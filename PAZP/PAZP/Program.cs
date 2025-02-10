368854a28fbc986125b1b4eb54ba90ae83ab3fe9
TheSite50 <theSite52@gmail.com>
2025-02-10 22:15:07 +0100

    Additional changes
    added weights

--- PAZP/PAZP/Program.cs
+++ PAZP/PAZP/Program.cs
@@ -6,6 +6,12 @@ using System.Linq;
 //problem komiwojażera
 
 
+//liczba iteracji
+//zrób array wejściowy
+//zsumiuj wartośćio
+//wylosuj nr komórki i nr jak tam wejdzie
+//porównaj
+//ten który ma mniejszy max przechodzi dalej
 
 namespace PAZP
 {
@@ -13,51 +19,138 @@ namespace PAZP
     {
         static void Main(string[] args)
         {
-            //liczba iteracji
-            //zrób array wejściowy
-            //zsumiuj wartośćio
-            //wylosuj nr komórki i nr jak tam wejdzie
-            //porównaj
-            //ten który ma mniejszy max przechodzi dalej
+            // Pobieranie liczby iteracji, procesów i procesorów od użytkownika
             Console.WriteLine("How Many Iterations?: ");
-            int iter = int.Parse(Console.ReadLine());
+            int iterations = int.Parse(Console.ReadLine());
+
             Console.WriteLine("How Many Processes?: ");
-            int ProcessCount = int.Parse(Console.ReadLine());
-            int[] parent = new int[ProcessCount];
+            int tasks = int.Parse(Console.ReadLine());
+            int[] taskArr = new int[tasks];
+
             Console.WriteLine("How Many Processors?: ");
-            int ProcessorNumber = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+            double[] workerSum = new double[workers];
+
+            // Tablica wag dla procesorów
+            double[] weights = { 1.0, 1.25, 1.5, 1.75 };
+
             Random rnd = new Random();
-            int[] sums = new int[ProcessorNumber];
-            for (int i = 0; i < ProcessCount; i++) 
+
+            // Przydzielanie procesów do losowych procesorów
+            for (int i = 0; i < tasks; i++)
             {
-                int curProc = rnd.Next(ProcessorNumber);
-                parent[i] = curProc;
-                sums[curProc] += i;
-                Console.Write(parent[i] + " ");
+                int currentWorker = rnd.Next(workers);
+                taskArr[i] = currentWorker;
+                workerSum[currentWorker] += i * weights[currentWorker];
+                Console.Write(taskArr[i] + " ");
             }
-            Console.Write(sums.Max());
+            Console.Write(workerSum.Max());
             Console.WriteLine();
-            //100
-            for (int i = 0; i < iter; i++)
+
+            // Główna pętla iteracji
+            for (int i = 0; i < iterations; i++)
             {
-                int next = rnd.Next(ProcessCount); //pozycja
-                int nextVal = rnd.Next(ProcessorNumber); //który procesor
-                int[] childSum = (int[])sums.Clone();
-                childSum[parent[next]] -= next;
-                childSum[nextVal] += next;
-                if(childSum.Max() < sums.Max()) 
+                // Wybór losowego zadania
+                int taskToBeSwapped = rnd.Next(tasks);
+
+                // Wybór losowego pracownika
+                int workerToBeInserted = rnd.Next(workers);
+
+                // Kopiowanie tablicy
+                double[] childSum = (double[])workerSum.Clone();
+
+                childSum[taskArr[taskToBeSwapped]] -= taskToBeSwapped * weights[taskArr[taskToBeSwapped]];
+                childSum[workerToBeInserted] += taskToBeSwapped * weights[workerToBeInserted];
+
+                // Aktualizacja jeśli nowy rozkład jest lepszy
+                if (childSum.Max() < workerSum.Max())
                 {
-                    sums[parent[next]] -= next;
-                    sums[nextVal] += next;
-                    parent[next] = nextVal;
+                    workerSum[taskArr[taskToBeSwapped]] -= taskToBeSwapped * weights[taskArr[taskToBeSwapped]];
+                    workerSum[workerToBeInserted] += taskToBeSwapped * weights[workerToBeInserted];
+                    taskArr[taskToBeSwapped] = workerToBeInserted;
                 }
             }
-            foreach (int i in parent) 
+
+            // Wypisanie wyniku
+            foreach (int i in taskArr)
             {
                 Console.Write(i + " ");
             }
-            Console.WriteLine(sums.Max());
+            Console.WriteLine(workerSum.Max());
             Console.ReadKey();
         }
     }
 }
+
+
+/*
+namespace PAZP
+{
+internal class Program
+{
+static void Main(string[] args)
+{
+
+//ces => proCESs    task
+//sor => procesSOR  worker
+//par => PARent
+
+
+// Pobieranie liczby iteracji, procesów i procesorów od użytkownika
+Console.WriteLine("How Many Iterations?: ");
+int iterations = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How Many Processes?: ");
+int tasks = int.Parse(Console.ReadLine());
+int[] taskArr = new int[tasks];
+
+Console.WriteLine("How Many Processors?: ");
+int workers = int.Parse(Console.ReadLine());
+double[] workerSum = new double[workers];
+
+// Tablica wag dla procesorów
+double[] weights = { 1.0, 1.25, 1.5, 1.75 };
+
+Random rnd = new Random();
+
+// Przydzielanie procesów do losowych procesorów
+for (int i = 0; i < tasks; i++) 
+{
+    int currentWorker = rnd.Next(workers);
+    taskArr[i] = currentWorker;
+    workerSum[currentWorker] += i * weights[currentWorker];
+    Console.Write(taskArr[i] + " ");
+}
+Console.Write(workerSum.Max());
+Console.WriteLine();
+//100
+for (int i = 0; i < iterations; i++)
+{
+    //select position
+    int TaskToBeSwapped = rnd.Next(tasks);
+    //select processor
+    int WorkerToBeInserted = rnd.Next(workers);
+    //copy the array
+    double[] childSum = (double[])workerSum.Clone();
+
+    childSum[taskArr[TaskToBeSwapped]] -= TaskToBeSwapped * weights[taskArr[TaskToBeSwapped]];
+    childSum[WorkerToBeInserted] += TaskToBeSwapped * weights[WorkerToBeInserted];
+
+    if(childSum.Max() < workerSum.Max()) 
+    {
+        workerSum[taskArr[TaskToBeSwapped]] -= TaskToBeSwapped * weights[taskArr[TaskToBeSwapped]];
+        workerSum[WorkerToBeInserted] += TaskToBeSwapped * weights[WorkerToBeInserted];
+        taskArr[TaskToBeSwapped] = WorkerToBeInserted;
+    }
+}
+//wypisanie wyniku
+foreach (int i in taskArr) 
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine(workerSum.Max());
+Console.ReadKey();
+}
+}
+}
+*/
